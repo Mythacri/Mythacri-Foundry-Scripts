28249732f62762c3ff59ec5d8d906bec6063e62a
Zhell <zhellqol@gmail.com>
2023-09-03 20:09:17 +0200

    [#5]

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,7 +1,9 @@
 import {Mayhem} from "./modules/data/mayhem.mjs";
+import {Resting} from "./modules/data/resting.mjs";
 import {PublicInterface} from "./modules/interface.mjs";
 import {SystemConfig} from "./modules/system-config.mjs";
 
 Hooks.once("init", SystemConfig.init);
 Hooks.once("init", PublicInterface.init);
 Hooks.once("init", Mayhem.init);
+Hooks.once("init", Resting.init);
