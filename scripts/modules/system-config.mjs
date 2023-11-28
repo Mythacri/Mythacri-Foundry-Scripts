4c8ab8286cdc978d87c2d2ec476cccf3fb063288
Mythacri <42121945+Mythacri@users.noreply.github.com>
2023-11-28 09:53:39 -0800

    Update scripts/modules/system-config.mjs
    Co-authored-by: Zhell <50169243+krbz999@users.noreply.github.com>

--- scripts/modules/system-config.mjs
+++ scripts/modules/system-config.mjs
@@ -131,7 +131,7 @@ export class SystemConfig {
 
     CONFIG.DND5E.weaponIds.cutlass = "mythacri-shared-compendium.equipment-myth.9r8gPk4RGDUUytZy";
     CONFIG.DND5E.weaponIds.estoc = "mythacri-shared-compendium.equipment-myth.xw77ea4rCFnpHgNy";
-    CONFIG.DND5E.weaponIds.Harpoon = "mythacri-shared-compendium.equipment-myth.EanNQeIbvCMz03w5";
+    CONFIG.DND5E.weaponIds.harpoon = "mythacri-shared-compendium.equipment-myth.EanNQeIbvCMz03w5";
     CONFIG.DND5E.weaponIds.throwingDagger = "mythacri-shared-compendium.equipment-myth.5fAnwS37xiOTwpOS";
 
     CONFIG.DND5E.weaponIds.Fishhook = "mythacri-shared-compendium.equipment-myth.lQqGpkcldeOKwZM0";
